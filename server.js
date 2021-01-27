99396390e0f147ff874c848bd558391c76ae808a
Omid Nassir <nassiromid@gmail.com>
2021-01-27 13:27:03 -0800

    Fixed issue with request param

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const mongoose = require('mongoose');
 const Todo = require('./model/todo');
 
 app.set('view engine', 'ejs');
+
+// Access css styles
 app.use(express.static('public'));
+
+// Parse url data from form and put into request object 
+app.use(express.urlencoded({extended: true}));
+
 
 const mongooseURI = 'mongodb+srv://rissandimo:q7iASCVlmTIsCq1R@cluster0.5y4lz.mongodb.net/todo-list?retryWrites=true&w=majority'
 mongoose.connect(mongooseURI, { useNewUrlParser: true, useUnifiedTopology: true})
@@ -13,17 +19,27 @@ mongoose.connect(mongooseURI, { useNewUrlParser: true, useUnifiedTopology: true}
     
     app.listen(3000);
     console.log('Listening for incoming requests');
-})
+}).catch(error => console.log(error));
 
 
 // App Routes
 app.get('/', (req, res) => {
     const todos = [
-        {title: 'First Todo', description: 'This is the first description'},
-        {title: 'Second Todo', description: 'This is the second description'},
-        {title: 'Third Todo', description: 'This is the third description'},
+        // {title: 'First Todo', description: 'This is the first description'},
+        // {title: 'Second Todo', description: 'This is the second description'},
+        // {title: 'Third Todo', description: 'This is the third description'},
     ]
     res.render('index', {title: 'All Todos', todos});
+})
+
+app.get('/add-todo', (req, res) => {
+    const todo = new Todo({
+        title: 'New Hard Coded Todo',
+        description: 'Some hard coded description'
+    });
+
+    todo.save().then((result) => res.send(result))
+    .catch(error => console.log(error));
 })
 
 app.get('/create-task', (req, res) => {
@@ -33,15 +49,17 @@ app.get('/create-task', (req, res) => {
 // Blog Routes
 app.post('/todos', (req, res) => {
 
+    console.log('Post request inititated');
+
     // Create new todo object
     const todo = new Todo(req.body);
 
     // Save blog to MongoDB
     todo.save()
-    .then(() => console.log('Todo saved to DB'))
+    .then(() => {
+        console.log('Todo saved to DB');
+        // Redirect back to Todos page
+        res.redirect('/');
+    })
     .catch(() => console.log(error => console.log(error)))
-
-    // Redirect back to Todos page
-    res.redirect('/');
-
 })
