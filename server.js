a398ad95113fce527738156f1a0c07b8f2d1709b
Omid Nassir <nassiromid@gmail.com>
2021-01-27 10:16:08 -0800

    Hard code test todos in server and pass to index page and render using ejs

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ app.use(express.static('public'));
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index', {title: 'All Todos'});
+    const todos = [
+        {title: 'First Todo', description: 'This is the first description'},
+        {title: 'Second Todo', description: 'This is the second description'},
+        {title: 'Third Todo', description: 'This is the third description'},
+    ]
+    res.render('index', {title: 'All Todos', todos});
 })
 
 app.get('/create-task', (req, res) => {
