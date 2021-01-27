96d80acc2c681986a28ca06ca2b6786973715df1
Omid Nassir <nassiromid@gmail.com>
2021-01-27 09:46:33 -0800

    Added styling to form

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.render('index', {title: 'All Todos'});
 })
 
-app.get('/todos/create-task', (req, res) => {
+app.get('/create-task', (req, res) => {
     res.render('create-task', {title: 'Create New Task'});
 })
