f82221732f5f1f29b7f5ec2616631ca76cf920b5
Omid Nassir <nassiromid@gmail.com>
2021-01-27 09:28:54 -0800

    Created new task form

--- server.js
+++ server.js
@@ -9,3 +9,7 @@ app.listen(3000);
 app.get('/', (req, res) => {
     res.render('index', {title: 'All Todos'});
 })
+
+app.get('/todos/create-task', (req, res) => {
+    res.render('create-task', {title: 'Create New Task'});
+})
