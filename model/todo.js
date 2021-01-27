95267bbdb7687ab02880b19543fb27f247ff2b6d
Omid Nassir <nassiromid@gmail.com>
2021-01-27 12:07:05 -0800

    Created Todo model & schema Connect to mongodb Save todo to database

---
+++ model/todo.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
+
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const Todo = require('./model/todo');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.listen(3000);
+const mongooseURI = 'mongodb+srv://rissandimo:q7iASCVlmTIsCq1R@cluster0.5y4lz.mongodb.net/todo-list?retryWrites=true&w=majority'
+mongoose.connect(mongooseURI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    console.log('Connected to DB');
+    
+    app.listen(3000);
+    console.log('Listening for incoming requests');
+})
 
+
+// App Routes
 app.get('/', (req, res) => {
     const todos = [
         {title: 'First Todo', description: 'This is the first description'},
@@ -17,4 +28,20 @@ app.get('/', (req, res) => {
 
 app.get('/create-task', (req, res) => {
     res.render('create-task', {title: 'Create New Task'});
+})
+
+// Blog Routes
+app.post('/todos', (req, res) => {
+
+    // Create new todo object
+    const todo = new Todo(req.body);
+
+    // Save blog to MongoDB
+    todo.save()
+    .then(() => console.log('Todo saved to DB'))
+    .catch(() => console.log(error => console.log(error)))
+
+    // Redirect back to Todos page
+    res.redirect('/');
+
 })
